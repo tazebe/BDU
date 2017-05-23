42d10a62263b322809e3bc0118c24aafa153173a
tazz <tazz@DESKTOP-NG1APEJ>
2017-05-23 10:52:23 -0800

    1

---
+++ src/tool/Tool.java
@@ -0,0 +1,13 @@
+
+package tool;
+
+import javax.swing.JOptionPane;
+
+public class Tool {
+
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Hey My Group members");
+    }
+    
+}
